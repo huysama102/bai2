ac4127f9cdaca1311c47bc72b0fa08c5e0f00338
vqh <huybacham0gmail.com>
2026-01-17 15:52:24 +0700

    hello world

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// Component Square
+const Square = () => {
+  return (
+    <View
+      style={{
+        width: 300,
+        height: 300,
+        backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Hello world</Text>
+      <Text> yêu em hiển vãi</Text>
+    </View>
+  );
+};
+
+// App chính
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Square />
+    </View>
+  );
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Thông tin sinh viên 
+- Họ và tên :Vũ Quang Huy
+- Mã sinh viên : 23810310290 
+
+hình ảnh 
